736fcd337d786e5fdc6c8f038b4677781fd54308
MJ Gomez-Saavedra <majogosaa98@gmail.com>
2023-12-12 23:58:51 -0500

    changes to word replacement

--- sketch.js
+++ sketch.js
@@ -6,13 +6,7 @@ let posterDone = false;
 
 let word = "HELLO";
 
-let posArr = [
-  [-10, -10],
-  [-10, -10],
-  [-10, -10],
-  [-10, -10],
-  [-10, -10],
-];
+let posArr = [];
 
 let t = 0;
 let capturedImage = null;
@@ -27,6 +21,7 @@ function setup() {
   canvas.parent("sketch-holder");
   setupCamera();
   setupPoseNet();
+  populatePosArray(word.length); // Initialize posArr with the starter word length
 }
 
 function setupCamera() {
@@ -70,27 +65,14 @@ function drawCamera() {
   } else {
     background("#FB877F");
 
-    // Create an image and draw the video onto it
-    capturedImage = createImage(width, height);
-    capturedImage.copy(
-      video,
-      0,
-      0,
-      video.width,
-      video.height,
-      0,
-      0,
-      width,
-      height
-    );
-
     // Display the captured thumbnail on the canvas
-    push();
-    translate(width - 150, height - 150);
-    scale(-1, 1);
-    image(capturedImage, -140, 15, 160, 120);
-    pop();
-
+    if (capturedImage) {
+      push();
+      translate(width - 150, height - 150);
+      scale(-1, 1);
+      image(capturedImage, -140, 15, 160, 120);
+      pop();
+    }
     posterDone = true;
   }
 }
@@ -110,9 +92,11 @@ function drawText() {
   for (let i = 0; i < poses.length; i++) {
     let pose = poses[i].pose;
     keypoint = pose.keypoints[0];
-    if (keypoint.score > 0.2) {
+
+    if (keypoint && keypoint.score > 0.2) {
       t = floor(millis() / 3000);
-      if (t < 5) {
+      if (t < word.length) {
+        // Ensure posArr is populated based on the number of letters in the word
         posArr[t] = [width - keypoint.position.x, keypoint.position.y];
       } else {
         // Capture the current frame as an image
@@ -123,9 +107,8 @@ function drawText() {
     }
 
     // Display the text
-    for (j = 0; j < word.length; j++) {
-      if (j < word.length) {
-        // Check if the index is within the length of the word
+    for (let j = 0; j < word.length; j++) {
+      if (posArr[j]) {
         text(word[j], posArr[j][0], posArr[j][1]);
       }
     }
@@ -134,7 +117,28 @@ function drawText() {
 
 function updateSketch() {
   // Get the input value from the HTML element
-  word = document.getElementById("inputValue").value;
+  let newWord = document.getElementById("inputValue").value;
+
+  // Update the word variable
+  word = newWord;
+
+  // Adjust posArr length based on the new word length
+  populatePosArray(newWord);
+
   // Redraw the canvas with the updated value
   redraw();
+}
+
+function populatePosArray(newWord) {
+  let newPosArray = [];
+
+  for (let i = 0; i < newWord.length; i++) {
+    if (posArr[i] && posArr[i][0] !== -70 && posArr[i][1] !== -70) {
+      newPosArray[i] = posArr[i];
+    } else {
+      newPosArray[i] = [-70, -70];
+    }
+  }
+
+  posArr = newPosArray;
 }
